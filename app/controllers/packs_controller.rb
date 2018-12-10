2bb61c8e62a2e91430458f492a7ef07ca4d873ed
David Sellam <dvdsellam@gmail.com>
2018-12-10 18:29:00 +0200

    routes pour packs

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -1,2 +1,11 @@
 class PacksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @packs = Pack.all
+  end
+
+  def show
+    @pack = Pack.find(params[:id])
+  end
 end
