a6962d4a3e9e71d993016e7559df9ad55f7a1a72
David Sellam <dvdsellam@gmail.com>
2018-12-10 20:33:01 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Pack.destroy_all
+puts "Je suis le Dieu du Timbre"
+
+CATEGORIES = ["Lettre Verte", "Lettre Prioritaire", "Lettre Suivie", "Personnalisé"]
+pack1 = Pack.create!(category: CATEGORIES[0])
+pack2 = Pack.create!(category: CATEGORIES[1])
+pack3 = Pack.create!(category: CATEGORIES[2])
+pack4 = Pack.create!(category: CATEGORIES[3])
+
+admin = User.create!(name: "Gabriel Zerbib", entreprise: "Timply", address: "4bis rue Benjamin Delessert", email: "gabrielzerbib6@gmail.com", password: "123456", admin: true)
+
+puts "C'est bon c'est fait on peut coder!"
