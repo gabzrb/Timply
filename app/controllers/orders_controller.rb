ab09aa6235c9ebaf08379aa0531a717870705824
David Sellam <dvdsellam@gmail.com>
2018-12-11 17:31:20 +0200

    admin verification

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
     before_action :set_order, only: [:show]
-
+    before_action :check_admin, only: [:index]
   def index
     @orders = Order.all
   end
@@ -9,12 +9,31 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+    @pack = Pack.find(params[:pack_id])
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.pack_id = @pack.id
+    if @order.save!
+      redirect_to :show
+    else
+      flash.now[:error] = "Oops, something went wrong. Please try again"
+      render :new
+    end
   end
 
   private
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
+
+  def check_admin
+    current_user.admin ? redirect_to :index : redirect_to packs_path
+  end
 
   def set_order
     @order =  Order.find(params[:id])
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,12 @@ User.destroy_all
 Pack.destroy_all
 puts "Je suis le Dieu du Timbre"
 
-CATEGORIES = ["Lettre Verte", "Lettre Prioritaire", "Lettre Suivie", "Personnalisé"]
-pack1 = Pack.create!(category: CATEGORIES[0])
-pack2 = Pack.create!(category: CATEGORIES[1])
-pack3 = Pack.create!(category: CATEGORIES[2])
-pack4 = Pack.create!(category: CATEGORIES[3])
+TITLES = ["Lettre Verte", "Lettre Prioritaire", "Lettre Suivie", "Recommandés"]
+
+pack1 = Pack.create!(title: TITLES[0])
+pack2 = Pack.create!(title: TITLES[1])
+pack3 = Pack.create!(title: TITLES[2])
+pack4 = Pack.create!(title: TITLES[3])
 
 admin = User.create!(name: "Gabriel Zerbib", entreprise: "Timply", address: "4bis rue Benjamin Delessert", email: "gabrielzerbib6@gmail.com", password: "123456", admin: true)
 
