42bd0bda5b97b43e4304a9da676f3134341f139a
David Sellam <dvdsellam@gmail.com>
2018-12-17 12:23:57 +0200

    trash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_164712) do
+ActiveRecord::Schema.define(version: 2018_12_11_155450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_12_10_164712) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "infos"
   end
 
   create_table "users", force: :cascade do |t|
