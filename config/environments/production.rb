3bf085d03a32b29062b47a5b9059f0178ae40c79
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-02-02 17:07:22 +0200

    heroku 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
@@ -77,6 +77,7 @@ Rails.application.configure do
     user_name: ENV['GMAIL_USERNAME'],
     password: ENV['GMAIL_PASSWORD']
   }
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
