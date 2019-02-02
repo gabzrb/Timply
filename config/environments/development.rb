0f9620032246e37fab21bf78b71460a2fd140a29
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-02-02 15:52:45 +0200

    mdpo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,23 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+
+
+  config.action_mailer.smtp_settings = {
+    address: “smtp.gmail.com”,
+    port: 587,
+    domain: ENV[“GMAIL_DOMAIN”],
+    authentication: “plain”,
+    enable_starttls_auto: true,
+    user_name: ENV[“GMAIL_USERNAME”],
+    password: ENV[“GMAIL_PASSWORD”]
+  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,26 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
