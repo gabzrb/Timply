457102dbf43af99ae7a694ed4406419784a6cf59
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-02-04 17:42:16 +0200

    plein de truc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,14 @@ puts "Je suis le Dieu du Timbre"
 
 TITLES = ["Lettre Verte", "Lettre Prioritaire", "Lettre Suivie", "Recommandés"]
 POIDS = ["20g", "100g", "250g", "500g", "3kg"]
-INFO_Vert = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit officiis a placeat, fugiat voluptatibus quos blanditiis consequuntur soluta atque error quis aliquam tenetur.
- Pour toute demande particulière (collecte de vos enveloppes ...) Contactez-nous'
+INFO_Vert = "La lettre verte est la plus économique et met entre 2 et 3 jours à arriver au destinataire. Le délai suite à votre commande varie en fonction de votre demande et est de maximum une semaine.
+ Pour toute demande particulière (collecte de vos enveloppes ...), Contactez-nous."
+INFO_Prio = "La lettre prioritaire vous permet d'envoyer un courrier urgent en moins de 24h. Le délai suite à votre commande varie en fonction de votre demande et est de maximum une semaine.
+ Pour toute demande particulière (collecte de vos enveloppes ...), Contactez-nous."
+INFO_Suivie = "La lettre suivie vous permet de suivre l'acheminement et la distribution de vos envois importants, documents ou petits objets en 48h. Le délai suite à votre commande varie en fonction de votre demande et est de maximum une semaine.
+ Pour toute demande particulière (collecte de vos enveloppes ...), Contactez-nous."
+INFO_Recomandé = "La lettre recomandée permettant à l'expéditeur d'un courrier de recevoir la preuve de sa bonne réception, signée par le récipiendaire. Le délai suite à votre commande varie en fonction de votre demande et est de maximum une semaine.
+ Pour toute demande particulière (collecte de vos enveloppes ...), Contactez-nous."
 DESCRIPTION_V = "À partir de 0,83€"
 DESCRIPTION_P = "À partir de 1€"
 DESCRIPTION_S = "À partir de 1,20€"
@@ -24,9 +30,9 @@ RECO = ["Pack R1", "Pack R2", "Pack R3", "Pack R1 Internationale", "Pack R2 Inte
 
 packvert = Pack.create!(title: TITLES[0], category:VERT, photo:"https://i.ibb.co/Z2Jjz9Y/Screen-Shot-2018-12-18-at-20-22-59.png", infos: INFO_Vert, description:DESCRIPTION_V, delivered:
                         DELIVERED_V )
-packprio = Pack.create!(title: TITLES[1], category:PRIO, photo: "https://i.ibb.co/RNMcFqG/Screen-Shot-2018-12-18-at-19-29-54.png",infos: INFO_Vert, description:DESCRIPTION_P, delivered:DELIVERED_P  )
-packsuivie = Pack.create!(title: TITLES[2], category:SUIVIE, photo: "https://i.ibb.co/KKLQdXy/Screen-Shot-2018-12-18-at-19-34-33.png",infos: INFO_Vert, description:DESCRIPTION_S, delivered:DELIVERED_S )
-packreco = Pack.create!(title: TITLES[3], category:RECO, photo: "https://i.ibb.co/NVnmv8p/Screen-Shot-2018-12-18-at-20-24-37.png",infos: INFO_Vert, description:DESCRIPTION_R, delivered:DELIVERED_R )
+packprio = Pack.create!(title: TITLES[1], category:PRIO, photo: "https://i.ibb.co/RNMcFqG/Screen-Shot-2018-12-18-at-19-29-54.png",infos: INFO_Prio, description:DESCRIPTION_P, delivered:DELIVERED_P  )
+packsuivie = Pack.create!(title: TITLES[2], category:SUIVIE, photo: "https://i.ibb.co/KKLQdXy/Screen-Shot-2018-12-18-at-19-34-33.png",infos: INFO_Suivie, description:DESCRIPTION_S, delivered:DELIVERED_S )
+packreco = Pack.create!(title: TITLES[3], category:RECO, photo: "https://i.ibb.co/NVnmv8p/Screen-Shot-2018-12-18-at-20-24-37.png",infos: INFO_Recomandé, description:DESCRIPTION_R, delivered:DELIVERED_R )
 
 admin = User.create!(name: "Gabriel Zerbib", entreprise: "Timply", address: "4bis rue Benjamin Delessert", email: "gabrielzerbib6@gmail.com", password: "123456", admin: true)
 admin = User.create!(name: "Ilan Millet", entreprise: "Timply", address: "12 rue Vivienne", email: "ilan.millet@gmail.com", password: "vestige", admin: true)
