6b1243c61cca82ff07be547c7f58141f3cb53769
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-02-07 09:05:10 +0200

    inscription mail2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+
 class UsersController < ApplicationController
   def create
     # Create the user from params
--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -3,7 +3,7 @@ class UserNotifierMailer < ApplicationMailer
 
 
   class UserNotifier < ActionMailer::Base
-    default :from => 'any_from_address@example.com'
+    default :from => 'gabrielzerbib6@gmail.com'
 
     # send a signup email to the user, pass in the user object that   contains the user's email address
     def send_signup_email(user)
