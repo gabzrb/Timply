dcc944635c5963aa85e60540806a499c5d23a5a0
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-03-20 13:21:59 +0200

    prenom inscription3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :orders
   validates :name, presence: true
-  validates :prenom, presence:true
+  validates :prenom, presence: true
   validates :address, presence: true
   after_create :send_welcome_email
 
