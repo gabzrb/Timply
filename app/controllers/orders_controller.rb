2d5da6a124d791674076f58b7e00b1507654c3a1
David Sellam <dvdsellam@gmail.com>
2019-04-11 15:14:01 +0300

    debug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,15 +34,15 @@ def create
     @order.pack_sku = @order.category.gsub(' ', '-')
     @order.state = 'pending'
     @order.price = (PACKS[@order.category.to_sym][@order.weight.to_sym].first.to_f)*@order.quantity.to_i if (@order.quantity && @order.weight && @order.category != "")
+    @order.reduction = Money.new(((PACKS[@order.category.to_sym][@order.weight.to_sym].last.to_f) * @order.quantity.to_i) * 100, 'EUR') - @order.price if (@order.quantity && @order.weight && @order.category != "")
     if ["Recommandée R1", "Recommandée R2", "Recommandée R3"].include?(@order.category) || @order.ar
       @order.price += Money.new(115, 'EUR')
-      @order.reduction = Money.new(115, 'EUR')
+      @order.reduction = Money.new(115, 'EUR') + Money.new(@order.reduction.to_f * 100, 'EUR')
     end
     if ["Recommandée R1 Internationale", "Recommandée R2 Internationale"].include?(@order.category) && @order.category
       @order.price += Money.new(140, 'EUR')
-      @order.reduction = Money.new(140, 'EUR')
+      @order.reduction = Money.new(140, 'EUR') + Money.new(@order.reduction.to_f * 100, 'EUR')
     end
-    @order.reduction += Money.new(((PACKS[@order.category.to_sym][@order.weight.to_sym].last.to_f)@order.quantity.to_i) 100, 'EUR') - @order.price if (@order.quantity && @order.weight && @order.category != "")
     @order.user_id = current_user.id
     @order.pack_id = @pack.id
     if @order.save
