31c530e489fd5ef56dd25e859d3691a1f656c846
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-08-04 07:41:19 +0300

    tarif + navnotconnected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   def create
     # Create the user from params
     @user = User.new(params[:user])
+    if @user.save
+      UserMailer.welcome(@user).deliver_now
+    end
   end
 end
+
