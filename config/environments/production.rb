07d6e8ccdbd0061dfd94e152ce7ce800424b5c28
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-08-14 17:02:49 +0300

    design card + responsive

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://timply.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://timply.fr" }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   # Verifies that versions and hashed value of the package contents in the project's package.json
