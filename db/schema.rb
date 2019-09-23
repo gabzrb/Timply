e10827e1d026420731178b738f41507b78c02ffc
David Sellam <37403593+dvd90@users.noreply.github.com>
2019-09-23 16:25:10 +0300

    Revert "Cleaning css js"

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_152941) do
+ActiveRecord::Schema.define(version: 2019_07_29_163916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
-  end
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
@@ -40,8 +33,6 @@ ActiveRecord::Schema.define(version: 2019_09_18_152941) do
     t.boolean "ar", default: false
     t.boolean "pb", default: false
     t.string "format"
-    t.bigint "cart_id"
-    t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["pack_id"], name: "index_orders_on_pack_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -81,5 +72,4 @@ ActiveRecord::Schema.define(version: 2019_09_18_152941) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "carts", "users"
 end
