442d83bf24e2a55ec0e6afe9fe748939790bb5ed
David Sellam <dvdsellam@gmail.com>
2020-04-25 18:41:24 +0300

    nos tarifs

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
 
-  # get '/tracking', to: 'pages#tracking'
-  # post '/trackingresult', to: 'pages#trackingresult'
   get '/mentionslegales', to: 'pages#mentionslegales'
   get '/conditionsgenerales', to: 'pages#conditionsgenerales'
   get '/apropos', to: 'pages#apropos'
+  get '/nostarifs', to: 'pages#nostarifs'
+
   resources :contacts, only: [:new, :create]
   get :order_confirmation, to: 'orders#order_confirmation'
   resources :orders, only: [ :index, :destroy, :update, :edit ]
