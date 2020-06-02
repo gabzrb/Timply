22756956b87a0b6107967e6acc0294620412baf7
David Sellam <dvdsellam@gmail.com>
2020-06-02 10:26:18 +0300

    updated searchkik

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,13 @@ class Order < ApplicationRecord
   validates :weight, presence: true
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1 }
   monetize :price_cents
+
+  def search_data
+    attributes.merge(
+      user_name: user(&:name)
+      user_email: user(&:email)
+      user_prenom: user(&:prenom)
+      user_phone: user(&:phone)
+    )
+  end
 end
