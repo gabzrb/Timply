e8c8df6c7999cc4a3a6d5a20460a1d60a17ea192
gabriel zerbib <gabrielzerbib6@gmail.com>
2020-06-03 17:12:47 +0300

    modify userinfo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,17 @@ class UsersController < ApplicationController
       UserMailer.welcome(@user).deliver_now
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :entreprise, :siret, :phone, :address, :prenom, :codepostal, :ville, :comfirm_data, :email)
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/nostarifs', to: 'pages#nostarifs'
 
   resources :contacts, only: [:new, :create]
+  resources :users, only: [:update]
   get :order_confirmation, to: 'orders#order_confirmation'
   resources :orders, only: [ :index, :destroy, :update, :edit ]
   resources :packs, only: [ :index, :show ] do
